f43eca76613f73704dd706cbf0bece4b0149de9a
Abisanth Mj <mabisanth@gmail.com>
2024-10-05 12:58:13 +0530

    Update

--- resources/views/sale_pos/create.blade.php
+++ resources/views/sale_pos/create.blade.php
@@ -1,279 +1,131 @@
 @extends('layouts.app')
 
-@section('title', 'POS')
+@section('title', __('sale.pos_sale'))
 
 @section('content')
+    <section class="content no-print">
+        <input type="hidden" id="amount_rounding_method" value="{{ $pos_settings['amount_rounding_method'] ?? '' }}">
+        @if (!empty($pos_settings['allow_overselling']))
+            <input type="hidden" id="is_overselling_allowed">
+        @endif
+        @if (session('business.enable_rp') == 1)
+            <input type="hidden" id="reward_point_enabled">
+        @endif
+        @php
+            $is_discount_enabled = $pos_settings['disable_discount'] != 1 ? true : false;
+            $is_rp_enabled = session('business.enable_rp') == 1 ? true : false;
+        @endphp
+        {!! Form::open([
+            'url' => action([\App\Http\Controllers\SellPosController::class, 'store']),
+            'method' => 'post',
+            'id' => 'add_pos_sell_form',
+        ]) !!}
+        <div class="row mb-12">
+            <div class="col-md-12 tw-pt-0 tw-mb-14">
+                <div class="row tw-flex lg:tw-flex-row md:tw-flex-col sm:tw-flex-col tw-flex-col tw-items-start md:tw-gap-4">
+                    {{-- <div class="@if (empty($pos_settings['hide_product_suggestion'])) col-md-7 @else col-md-10 col-md-offset-1 @endif no-padding pr-12"> --}}
+                    <div class="tw-px-3 tw-w-full lg:tw-w-[70%] lg:tw-px-0 lg:tw-pr-0">
 
-<!-- Content Header (Page header) -->
-<!-- <section class="content-header">
-    <h1>Add Purchase</h1> -->
-    <!-- <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-    </ol> -->
-<!-- </section> -->
+                        <div class="tw-shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] tw-rounded-2xl tw-bg-white tw-mb-2 md:tw-mb-8 tw-p-2">
 
-<!-- Main content -->
-<section class="content no-print">
-	@if(!empty($pos_settings['allow_overselling']))
-		<input type="hidden" id="is_overselling_allowed">
-	@endif
-	@if(session('business.enable_rp') == 1)
-        <input type="hidden" id="reward_point_enabled">
+                            {{-- <div class="box box-solid mb-12 @if (!isMobile()) mb-40 @endif"> --}}
+                                <div class="box-body pb-0">
+                                    {!! Form::hidden('location_id', $default_location->id ?? null, [
+                                        'id' => 'location_id',
+                                        'data-receipt_printer_type' => !empty($default_location->receipt_printer_type)
+                                            ? $default_location->receipt_printer_type
+                                            : 'browser',
+                                        'data-default_payment_accounts' => $default_location->default_payment_accounts ?? '',
+                                    ]) !!}
+                                    <!-- sub_type -->
+                                    {!! Form::hidden('sub_type', isset($sub_type) ? $sub_type : null) !!}
+                                    <input type="hidden" id="item_addition_method"
+                                        value="{{ $business_details->item_addition_method }}">
+                                    @include('sale_pos.partials.pos_form')
+
+                                    @include('sale_pos.partials.pos_form_totals')
+
+                                    @include('sale_pos.partials.payment_modal')
+
+                                    @if (empty($pos_settings['disable_suspend']))
+                                        @include('sale_pos.partials.suspend_note_modal')
+                                    @endif
+
+                                    @if (empty($pos_settings['disable_recurring_invoice']))
+                                        @include('sale_pos.partials.recurring_invoice_modal')
+                                    @endif
+                                </div>
+                            {{-- </div> --}}
+                        </div>
+                    </div>
+                    @if (empty($pos_settings['hide_product_suggestion']) && !isMobile())
+                        <div class="md:tw-no-padding tw-w-full lg:tw-w-[40%] tw-px-5">
+                            @include('sale_pos.partials.pos_sidebar')
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+        @include('sale_pos.partials.pos_form_actions')
+        {!! Form::close() !!}
+    </section>
+
+    <!-- This will be printed -->
+    <section class="invoice print_section" id="receipt_section">
+    </section>
+    <div class="modal fade contact_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+        @include('contact.create', ['quick_add' => true])
+    </div>
+    @if (empty($pos_settings['hide_product_suggestion']) && isMobile())
+        @include('sale_pos.partials.mobile_product_suggestions')
     @endif
-	<div class="row">
-		<div class="@if(!empty($pos_settings['hide_product_suggestion']) && !empty($pos_settings['hide_recent_trans'])) col-md-10 col-md-offset-1 @else col-md-7 @endif col-sm-12">
-			@component('components.widget', ['class' => 'box-success'])
-				@slot('header')
-					<div class="col-sm-6">
-						<h3 class="box-title">POS Terminal <i class="fa fa-keyboard-o hover-q text-muted" aria-hidden="true" data-container="body" data-toggle="popover" data-placement="bottom" data-content="@include('sale_pos.partials.keyboard_shortcuts_details')" data-html="true" data-trigger="hover" data-original-title="" title=""></i></h3>
-					</div>
-					<div class="col-sm-6">
-						<p class="text-right"><strong>@lang('sale.location'):</strong> {{$default_location->name}}</p>
-					</div>
-					<input type="hidden" id="item_addition_method" value="{{$business_details->item_addition_method}}">
-				@endslot
-				{!! Form::open(['url' => action([\App\Http\Controllers\SellPosController::class, 'store']), 'method' => 'post', 'id' => 'add_pos_sell_form' ]) !!}
+    <!-- /.content -->
+    <div class="modal fade register_details_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+    </div>
+    <div class="modal fade close_register_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+    </div>
+    <!-- quick product modal -->
+    <div class="modal fade quick_add_product_modal" tabindex="-1" role="dialog" aria-labelledby="modalTitle"></div>
 
-				{!! Form::hidden('location_id', $default_location->id, ['id' => 'location_id', 'data-receipt_printer_type' => !empty($default_location->receipt_printer_type) ? $default_location->receipt_printer_type : 'browser', 'data-default_accounts' => $default_location->default_payment_accounts]); !!}
+    <div class="modal fade" id="expense_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+    </div>
 
-				<!-- /.box-header -->
-				<div class="box-body">
-					<div class="row">
-						@if(!empty($pos_settings['enable_transaction_date']))
-							<div class="col-md-4 col-sm-6">
-								<div class="form-group">
-									{!! Form::label('transaction_date', __('sale.sale_date') . ':*') !!}
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="fa fa-calendar"></i>
-										</span>
-										{!! Form::text('transaction_date', $default_datetime, ['class' => 'form-control', 'readonly', 'required']); !!}
-									</div>
-								</div>
-							</div>
-						@endif
-						@if(config('constants.enable_sell_in_diff_currency') == true)
-							<div class="col-md-4 col-sm-6">
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="fas fa-exchange-alt"></i>
-										</span>
-										{!! Form::text('exchange_rate', config('constants.currency_exchange_rate'), ['class' => 'form-control input-sm input_number', 'placeholder' => __('lang_v1.currency_exchange_rate'), 'id' => 'exchange_rate']); !!}
-									</div>
-								</div>
-							</div>
-						@endif
-						@if(!empty($price_groups) && count($price_groups) > 1)
-							<div class="col-md-4 col-sm-6">
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="fas fa-money-bill-alt"></i>
-										</span>
-										@php
-											reset($price_groups);
-											$selected_price_group = !empty($default_price_group_id) && array_key_exists($default_price_group_id, $price_groups) ? $default_price_group_id : null;
-										@endphp
-										{!! Form::hidden('hidden_price_group', key($price_groups), ['id' => 'hidden_price_group']) !!}
-										{!! Form::select('price_group', $price_groups, $selected_price_group, ['class' => 'form-control select2', 'id' => 'price_group', 'style' => 'width: 100%;']); !!}
-										<span class="input-group-addon">
-											@show_tooltip(__('lang_v1.price_group_help_text'))
-										</span> 
-									</div>
-								</div>
-							</div>
-						@else
-							@php
-								reset($price_groups);
-							@endphp
-							{!! Form::hidden('price_group', key($price_groups), ['id' => 'price_group']) !!}
-						@endif
-						@if(!empty($default_price_group_id))
-							{!! Form::hidden('default_price_group', $default_price_group_id, ['id' => 'default_price_group']) !!}
-						@endif
+    @include('sale_pos.partials.configure_search_modal')
 
-						@if(in_array('types_of_service', $enabled_modules) && !empty($types_of_service))
-							<div class="col-md-4 col-sm-6">
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="fa fa-external-link text-primary service_modal_btn"></i>
-										</span>
-										{!! Form::select('types_of_service_id', $types_of_service, null, ['class' => 'form-control', 'id' => 'types_of_service_id', 'style' => 'width: 100%;', 'placeholder' => __('lang_v1.select_types_of_service')]); !!}
+    @include('sale_pos.partials.recent_transactions_modal')
 
-										{!! Form::hidden('types_of_service_price_group', null, ['id' => 'types_of_service_price_group']) !!}
-
-										<span class="input-group-addon">
-											@show_tooltip(__('lang_v1.types_of_service_help'))
-										</span> 
-									</div>
-									<small><p class="help-block hide" id="price_group_text">@lang('lang_v1.price_group'): <span></span></p></small>
-								</div>
-							</div>
-							<div class="modal fade types_of_service_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel"></div>
-						@endif
-
-						@if(in_array('subscription', $enabled_modules))
-							<div class="col-md-4 pull-right col-sm-6">
-								<div class="checkbox">
-									<label>
-						              {!! Form::checkbox('is_recurring', 1, false, ['class' => 'input-icheck', 'id' => 'is_recurring']); !!} @lang('lang_v1.subscribe')?
-						            </label><button type="button" data-toggle="modal" data-target="#recurringInvoiceModal" class="btn btn-link"><i class="fa fa-external-link"></i></button>@show_tooltip(__('lang_v1.recurring_invoice_help'))
-								</div>
-							</div>
-						@endif
-					</div>
-					<div class="row">
-						<div class="@if(!empty($commission_agent)) col-sm-4 @else col-sm-6 @endif">
-							<div class="form-group" style="width: 100% !important">
-								<div class="input-group">
-									<span class="input-group-addon">
-										<i class="fa fa-user"></i>
-									</span>
-									<input type="hidden" id="default_customer_id" 
-									value="{{ $walk_in_customer['id']}}" >
-									<input type="hidden" id="default_customer_name" 
-									value="{{ $walk_in_customer['name']}}" >
-									{!! Form::select('contact_id', 
-										[], null, ['class' => 'form-control mousetrap', 'id' => 'customer_id', 'placeholder' => 'Enter Customer name / phone', 'required', 'style' => 'width: 100%;']); !!}
-									<span class="input-group-btn">
-										<button type="button" class="btn btn-default bg-white btn-flat add_new_customer" data-name=""  @if(!auth()->user()->can('customer.create')) disabled @endif><i class="fa fa-plus-circle text-primary fa-lg"></i></button>
-									</span>
-								</div>
-							</div>
-						</div>
-						<input type="hidden" name="pay_term_number" id="pay_term_number" value="{{$walk_in_customer['pay_term_number']}}">
-						<input type="hidden" name="pay_term_type" id="pay_term_type" value="{{$walk_in_customer['pay_term_type']}}">
-						
-						@if(!empty($commission_agent))
-							<div class="col-sm-4">
-								<div class="form-group">
-								{!! Form::select('commission_agent', 
-											$commission_agent, null, ['class' => 'form-control select2', 'placeholder' => __('lang_v1.commission_agent')]); !!}
-								</div>
-							</div>
-						@endif
-
-						<div class="@if(!empty($commission_agent)) col-sm-4 @else col-sm-6 @endif">
-							<div class="form-group">
-								<div class="input-group">
-									<div class="input-group-btn">
-										<button type="button" class="btn btn-default bg-white btn-flat" data-toggle="modal" data-target="#configure_search_modal" title="{{__('lang_v1.configure_product_search')}}"><i class="fa fa-barcode"></i></button>
-									</div>
-									{!! Form::text('search_product', null, ['class' => 'form-control mousetrap', 'id' => 'search_product', 'placeholder' => __('lang_v1.search_product_placeholder'),
-									'disabled' => is_null($default_location)? true : false,
-									'autofocus' => is_null($default_location)? false : true,
-									]); !!}
-									<span class="input-group-btn">
-										<button type="button" class="btn btn-default bg-white btn-flat pos_add_quick_product" data-href="{{action([\App\Http\Controllers\ProductController::class, 'quickAdd'])}}" data-container=".quick_add_product_modal"><i class="fa fa-plus-circle text-primary fa-lg"></i></button>
-									</span>
-								</div>
-							</div>
-						</div>
-						<div class="clearfix"></div>
-
-						<!-- Call restaurant module if defined -->
-				        @if(in_array('tables' ,$enabled_modules) || in_array('service_staff' ,$enabled_modules))
-				        	<span id="restaurant_module_span">
-				          		<div class="col-md-3"></div>
-				        	</span>
-				        @endif
-			        </div>
-
-					<div class="row">
-					<div class="col-sm-12 pos_product_div">
-						<input type="hidden" name="sell_price_tax" id="sell_price_tax" value="{{$business_details->sell_price_tax}}">
-
-						<!-- Keeps count of product rows -->
-						<input type="hidden" id="product_row_count" 
-							value="0">
-						@php
-							$hide_tax = '';
-							if( session()->get('business.enable_inline_tax') == 0){
-								$hide_tax = 'hide';
-							}
-						@endphp
-						<table class="table table-condensed table-bordered table-striped table-responsive" id="pos_table">
-							<thead>
-								<tr>
-									<th class="tex-center @if(!empty($pos_settings['inline_service_staff'])) col-md-3 @else col-md-4 @endif">	
-										@lang('sale.product') @show_tooltip(__('lang_v1.tooltip_sell_product_column'))
-									</th>
-									<th class="text-center col-md-3">
-										@lang('sale.qty')
-									</th>
-									@if(!empty($pos_settings['inline_service_staff']))
-										<th class="text-center col-md-2">
-											@lang('restaurant.service_staff')
-										</th>
-									@endif
-									<th class="text-center col-md-2 {{$hide_tax}}">
-										@lang('sale.price_inc_tax')
-									</th>
-									<th class="text-center col-md-2">
-										@lang('sale.subtotal')
-									</th>
-									<th class="text-center"><i class="fa fa-close" aria-hidden="true"></i></th>
-								</tr>
-							</thead>
-							<tbody></tbody>
-						</table>
-						</div>
-					</div>
-					@include('sale_pos.partials.pos_details')
-
-					@include('sale_pos.partials.payment_modal')
-
-					@if(empty($pos_settings['disable_suspend']))
-						@include('sale_pos.partials.suspend_note_modal')
-					@endif
-
-					@if(empty($pos_settings['disable_recurring_invoice']))
-						@include('sale_pos.partials.recurring_invoice_modal')
-					@endif
-				</div>
-				<!-- /.box-body -->
-				{!! Form::close() !!}
-			@endcomponent
-		</div>
-
-		<div class="col-md-5 col-sm-12">
-			@include('sale_pos.partials.right_div')
-		</div>
-	</div>
-</section>
-
-<!-- This will be printed -->
-<section class="invoice print_section" id="receipt_section">
-</section>
-<div class="modal fade contact_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-	@include('contact.create', ['quick_add' => true])
-</div>
-<!-- /.content -->
-<div class="modal fade register_details_modal" tabindex="-1" role="dialog" 
-	aria-labelledby="gridSystemModalLabel">
-</div>
-<div class="modal fade close_register_modal" tabindex="-1" role="dialog" 
-	aria-labelledby="gridSystemModalLabel">
-</div>
-<!-- quick product modal -->
-<div class="modal fade quick_add_product_modal" tabindex="-1" role="dialog" aria-labelledby="modalTitle"></div>
-
-@include('sale_pos.partials.configure_search_modal')
+    @include('sale_pos.partials.weighing_scale_modal')
 
 @stop
-
+@section('css')
+    <!-- include module css -->
+    @if (!empty($pos_module_data))
+        @foreach ($pos_module_data as $key => $value)
+            @if (!empty($value['module_css_path']))
+                @includeIf($value['module_css_path'])
+            @endif
+        @endforeach
+    @endif
+@stop
 @section('javascript')
-	<script src="{{ asset('js/pos.js?v=' . $asset_v) }}"></script>
-	<script src="{{ asset('js/printer.js?v=' . $asset_v) }}"></script>
-	<script src="{{ asset('js/product.js?v=' . $asset_v) }}"></script>
-	<script src="{{ asset('js/opening_stock.js?v=' . $asset_v) }}"></script>
-	@include('sale_pos.partials.keyboard_shortcuts')
+    <script src="{{ asset('js/pos.js?v=' . $asset_v) }}"></script>
+    <script src="{{ asset('js/printer.js?v=' . $asset_v) }}"></script>
+    <script src="{{ asset('js/product.js?v=' . $asset_v) }}"></script>
+    <script src="{{ asset('js/opening_stock.js?v=' . $asset_v) }}"></script>
+    @include('sale_pos.partials.keyboard_shortcuts')
 
-	<!-- Call restaurant module if defined -->
-    @if(in_array('tables' ,$enabled_modules) || in_array('modifiers' ,$enabled_modules) || in_array('service_staff' ,$enabled_modules))
-    	<script src="{{ asset('js/restaurant.js?v=' . $asset_v) }}"></script>
+    <!-- Call restaurant module if defined -->
+    @if (in_array('tables', $enabled_modules) ||
+            in_array('modifiers', $enabled_modules) ||
+            in_array('service_staff', $enabled_modules))
+        <script src="{{ asset('js/restaurant.js?v=' . $asset_v) }}"></script>
+    @endif
+    <!-- include module js -->
+    @if (!empty($pos_module_data))
+        @foreach ($pos_module_data as $key => $value)
+            @if (!empty($value['module_js_path']))
+                @includeIf($value['module_js_path'], ['view_data' => $value['view_data']])
+            @endif
+        @endforeach
     @endif
 @endsection
